88f4d866dbb0870dbea746c182b20f0a8d1516e9
Sergey Kostyuchenko <derfenix@gmail.com>
2017-07-16 13:49:20 +0300

    cleanup

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ conf_files='zshrc zprofile zshenv'
 
 for file in $conf_files; do
 		  if [ -e "${HOME}/.${file}" ] || [ -L "${HOME}/.${file}" ]; then
-					 echo 101
 					 mv -f "${HOME}/.${file}" "${HOME}/.${file}.old"
 		  fi
 		  ln -s "${ZSH_SOURCE}${file}" "${HOME}/.${file}"
